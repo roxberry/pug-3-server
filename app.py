ca6ee17400fe3dfd027b73b2b8246d8378550ace
Mark Roxberry <developer@driveapplied.com>
2017-08-13 19:38:04 -0400

    changed value to 6

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods={'GET'})
 def api_root():
     data = {
-        'hello': 'world-v5',
+        'hello': 'world-v6',
         'number': 6
     }
     resp = jsonify(data)
